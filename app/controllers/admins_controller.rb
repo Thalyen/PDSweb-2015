bd4d20cfbec2cc904a8c4b42b44a7f353109058b
GercinoJr <gercinojr@gmail.com>
2015-08-10 19:40:43 -0300

    criando area administrativa

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,10 @@
+class AdminsController < ApplicationController
+  
+	def index
+  		render layout: "area_administrativa"
+  	end
+
+  	def sign_in
+  		
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,8 @@ Rails.application.routes.draw do
 
   # rotas para os períodos
   get 'lancamentos_contabeis/periodo/:periodo' => 'lancamentos_contabeis#no_periodo'
+
+  # area administrativa
+  get '/admin/', to: 'admins#index'
+  get '/admin/sign_in', to: 'admins#sign_in'
 end
